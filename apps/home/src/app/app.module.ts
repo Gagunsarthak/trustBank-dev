cfc605da0465d94e6c5767f9e3fce8b260db3b5e
Gagunsarthak <saisarthakmohanty1996@gmail.com>
2022-01-15 11:53:39 +0530

    library component import

--- apps/home/src/app/app.module.ts
+++ apps/home/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatSliderModule } from '@angular/material/slider';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import {MyLibModule} from 'libs/my-lib/src/lib/my-lib.module'
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NxWelcomeComponent,],
+  imports: [BrowserModule,MyLibModule,MatSliderModule],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ libs/my-lib/src/lib/my-lib.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [
+    TopNavBarComponent
+  ],
+  exports:[TopNavBarComponent]
+})
+export class MyLibModule {}
